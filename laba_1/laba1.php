258ee631d3830b1352ce82e2fe33abda909fe2d8
98ryba4ok <158596812+98ryba4ok@users.noreply.github.com>
2024-02-12 22:09:34 +0300

    Add files via upload
    Laba_1

---
+++ laba_1/laba1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+<head> 
+    <meta charset="UTF-8"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title>Hello world!</title> 
+    <link rel="stylesheet" href="index2.css"> 
+</head> 
+<body> 
+ 
+ <header> 
+    <img  class= "logo" src="logo.png" alt="logo" height="100px">  
+    <h1 class="text"> Домашняя работа: Hello, World! </h1> 
+ </header>    
+ 
+<main> 
+<?php echo "Hello World!"; 
+?> 
+ 
+</main> 
+ 
+ 
+ <footer> 
+    <h2>задание для самостоятельно работы</h2> 
+ </footer> 
+ 
+ 
+</body> 
+</html>
